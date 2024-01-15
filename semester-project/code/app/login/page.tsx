671f9cf4dbd41ad48962583d0e4420fbb585a20b
jerkocurkovic <jerko.curkovic1@gmail.com>
2024-01-15 18:12:59 +0100

    Modified LogIn page

--- semester-project/code/app/login/page.tsx
+++ semester-project/code/app/login/page.tsx
@@ -25,7 +25,7 @@ function LogIn(){
         </main>*/
         <div className="flex flex-col max-w-[850px] min-w-[300px] xl:max-w-[850px] mx-auto bg-white overflow-hidden mb-10">
             
-            <div className="flex-1 flex flex-col justify-between px-6 py-4">
+            <div className="flex-1 flex flex-col justify-between px-6 py-4 text-center">
             <div className="mb-8">
                 <h2 className="text-2xl font-abeezee font-bold text-brand-black-500 mb-4">
                 Our sincere apologies!
