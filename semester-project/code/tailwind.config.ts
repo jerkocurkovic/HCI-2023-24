6487877bdf8d63577fc5df0ffb6eee6b8e11ac83
jerkocurkovic <jerko.curkovic1@gmail.com>
2024-01-25 20:00:00 +0100

    Added additional darker shades of colors

--- semester-project/code/tailwind.config.ts
+++ semester-project/code/tailwind.config.ts
@@ -23,15 +23,19 @@ const config: Config = {
         },
         "brand-black": {
           500: "#161615",
+          600: "#0e0e0e",
         },
         "brand-yellow": {
           500: "#F7CB14",
+          600: "#d9a10b",
         },
         "brand-blue": {
           500: "#255DA8",
+          600: "#1f4c84",
         },
         "brand-brown": {
           500: "#3A2404",
+          600: "#2c1b03",
         },
         "brand-grey": {
           50: "#D9D9D9",
