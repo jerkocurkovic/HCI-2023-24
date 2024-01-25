098b246e67d194e27f6abb8a556b502a2072267e
jerkocurkovic <jerko.curkovic1@gmail.com>
2024-01-25 19:57:02 +0100

    Added additional colors to button

--- semester-project/code/components/Button.tsx
+++ semester-project/code/components/Button.tsx
@@ -7,6 +7,10 @@ type ButtonProps = {
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
   green?: boolean;
   grey?: boolean;
+  black?: boolean;
+  blue?: boolean;
+  yellow?: boolean;
+  brown?: boolean;
   className?: string;
   iconClassName?: string;
 };
@@ -18,6 +22,10 @@ const Button = ({
   onClick,
   green,
   grey,
+  black,
+  blue,
+  yellow,
+  brown,
 }: ButtonProps) => {
   const buttonClasses = cn(
     "transition duration-300 ease-in-out transform group-hover:translate-x-1",
@@ -26,7 +34,16 @@ const Button = ({
     {
       "bg-brand-green-500 hover:bg-brand-green-600 text-white":
         green,
-      "bg-brand-grey-50 hover:bg-brand-grey-500": grey,
+      "bg-brand-grey-50 hover:bg-brand-grey-500": 
+        grey,
+      "bg-brand-black-500 hover:bg-brand-black-600 text-white": black,
+      "bg-brand-blue-500 hover:bg-brand-blue-600 text-white":
+        blue,
+      "bg-brand-yellow-500 hover:bg-brand-yellow-600 text-white":
+        yellow,
+      "bg-brand-brown-500 hover:bg-brand-brown-600 text-white":
+        brown,
+
     },
     className
   );
